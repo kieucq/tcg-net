b3016113098fe2831ae102c3249a6f6c7e23c41a
kieucq <kieucq@gmail.com>
2026-09-16 14:28:35 -0400

    renamed prediction -> inference for consistency with readme

--- models/prediction/utilities/datetime.py
+++
@@ -1,6 +0,0 @@
-import datetime
-import numpy as np
-
-def convert_datetime64_to_datetime( usert: np.datetime64 )->datetime.datetime:
-    t = np.datetime64( usert, 'us').astype(datetime.datetime)
-    return t
