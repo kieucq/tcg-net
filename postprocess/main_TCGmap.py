0c76d4a2e141bfdded2767bafb7f713e903e28ea
kieucq <kieucq@gmail.com>
2026-09-17 08:35:59 -0400

    updated the post-process so it can handle the NA basin as well

---
+++ postprocess/main_TCGmap.py
@@ -0,0 +1,6 @@
+# from Preprocess_Ibtracs_Merra2 import Preprocess_progress
+#from plot_TCGmap import plot_dynamic
+from plot_TCGmap import plot_dynamic
+
+if __name__ == "__main__":
+    plot_dynamic()
---
+++ postprocess/main_TCGmap_stat.py
@@ -0,0 +1,6 @@
+"""Generate all-case TCG probability statistics maps."""
+
+from plot_TCGmap_stat import plot_all_case_statistics
+
+if __name__ == "__main__":
+    plot_all_case_statistics()
